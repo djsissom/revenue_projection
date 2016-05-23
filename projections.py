fa9bc8dec4ef1edf223647176ee417c396a8b9e6
Daniel Sissom <djsissom@gmail.com>
2016-05-23 18:22:53 -0500

    Initial commit with python plotting boilerplate.

---
+++ projections.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python
+
+import sys
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from ipdb import set_trace
+
+
+
+def main():
+	pass
+
+
+
+if __name__ == '__main__':
+	main()
+
