f507bbccbb306a566b8c0fc3875cb25c4b160cc8
Daniel Sissom <djsissom@gmail.com>
2016-06-09 19:21:57 -0500

    Added net income calculations to revenue class.

--- projections.py
+++ projections.py
@@ -19,7 +19,9 @@ def main():
 	'''
 
 	# get arguments/filenames
-	config_file = sys.argv[1]
+	config_file = 'settings.conf'
+	if len(sys.argv > 1):
+		config_file = sys.argv[1]
 
 	# read config file
 	settings = Settings(config_file)
@@ -40,13 +42,72 @@ def main():
 
 class Revenue:
 	def __init__(self, settings):
-		fields = ['cost', 'months', 'conservative', 'estimate', 'aggressive']
-		self.fixed_cost_header,    self.fixed_costs    = read_files(settings.fixed_costs_file, header_line=0, rec_array=True, fields=fields)
-		fields = ['cost', 'months', 'customers', 'conservative', 'estimate', 'aggressive']
-		self.variable_cost_header, self.variable_costs = read_files(settings.variable_costs_file, header_line=0, rec_array=True, fields=fields)
-		fields = ['income', 'months', 'customers', 'conservative', 'estimate', 'aggressive']
-		self.income_header,        self.income         = read_files(settings.income_file, header_line=0, rec_array=True, fields=fields)
+		fields = (['cost', 'month_rate', 'conservative', 'estimate', 'aggressive'],
+		          ['cost', 'month_rate', 'customer_rate', 'conservative', 'estimate', 'aggressive'],
+		          ['income', 'month_rate', 'customer_rate', 'conservative', 'estimate', 'aggressive'])
+		self.fixed_costs    = read_files(settings.fixed_costs_file,    rec_array=True, fields=fields[0])
+		self.variable_costs = read_files(settings.variable_costs_file, rec_array=True, fields=fields[1])
+		self.income         = read_files(settings.income_file,         rec_array=True, fields=fields[2])
 		del fields
+
+
+	def net(self, months, customers, estimate_type='estimate'):
+		net = gross_income(months, customers, estimate_type) - expenses(months, customers, estimate_type)
+		return net
+
+
+	def gross_income(self, months, customers, estimate_type):
+		if estimate_type == 'estimate':
+			income = self.income.estimate
+		elif estimate_type == 'conservative':
+			income = self.income.conservative
+		elif estimate_type = 'aggressive':
+			income = self.income.aggressive
+
+		month_rate = self.income.month_rate
+		customer_rate = self.income.customer_rate
+
+		line_income = income * (months / month_rate) * np.floor(customers / customer_rate)
+		gross_income = np.sum(line_income)
+		return gross_income
+
+
+	def expenses(self, months, customers, estimate_type):
+		expenses = fixed_expenses(months, estimate_type) + variable_expenses(months, customers, estimate_type)
+		return expenses
+
+
+	def fixed_expenses(self, months, estimate_type):
+		if estimate_type == 'estimate':
+			fixed_costs = self.fixed_costs.estimate
+		elif estimate_type == 'conservative':
+			fixed_costs = self.fixed_costs.conservative
+		elif estimate_type = 'aggressive':
+			fixed_costs = self.fixed_costs.aggressive
+
+		month_rate = self.fixed_costs.month_rate
+
+		line_costs = fixed_costs * (months / month_rate)
+		expenses = np.sum(line_costs)
+
+		return expenses
+
+
+	def variable_expenses(self, months, customers, estimate_type):
+		if estimate_type == 'estimate':
+			variable_costs = self.variable_costs.estimate
+		elif estimate_type == 'conservative':
+			variable_costs = self.variable_costs.conservative
+		elif estimate_type = 'aggressive':
+			variable_costs = self.variable_costs.aggressive
+
+		month_rate = self.variable_costs.month_rate
+		customer_rate = self.variable_costs.customer_rate
+
+		line_costs = variable_costs * (months / month_rate) * np.floor(customers / customer_rate)
+		expenses = np.sum(line_costs)
+
+		return expenses
 
 
 
@@ -86,7 +147,7 @@ def read_files(files, header_line=None, comment_char='#', rec_array=False, field
 		header = header[1:]
 		header = header.split()
 
-	if fields == None and header != None:
+	if rec_array and fields == None and header != None:
 		fields = header
 
 	for file in files:
@@ -111,28 +172,53 @@ def read_files(files, header_line=None, comment_char='#', rec_array=False, field
 
 
 
-def make_plots(revenue):
+def make_plots(revenue, settings):
 	fig = make_time_plot(revenue)
 	fig = make_customer_plot(revenue)
 	fig = make_customer_time_plot(revenue)
 
 
 
-def make_time_plot(revenue):
-	fig = plt.figure(figsize = (9.0, 6.0))
+def make_time_plot(revenue, settings):
+	fig, ax = setup_plot()
+	ax = make_line_plot(ax, x_data, y_data)
+	fig = save_plot(fig, plot_name)
 	return fig
 
 
 
-def make_customer_plot(revenue):
-	fig = plt.figure(figsize = (9.0, 6.0))
+def make_customer_plot(revenue, settings):
+	fig, ax = setup_plot()
+	fig = save_plot(fig, plot_name)
 	return fig
 
 
 
-def make_customer_time_plot(revenue):
-	fig = plt.figure(figsize = (9.0, 6.0))
+def make_customer_time_plot(revenue, settings):
+	fig, ax = setup_plot()
+	fig = save_plot(fig, plot_name)
 	return fig
+
+
+
+def setup_plot():
+	print('Making plot...')
+	fig = plt.figure(figsize = (9.0, 6.0))
+	ax = fig.add_subplot(111, aspect='equal')
+	return fig, ax
+
+
+
+def save_plot(fig, name):
+	print('Saving %s plot.' % name)
+	fig.tight_layout()
+	plt.savefig(name, bbox_inches='tight')
+	return fig
+
+
+
+def make_line_plot(ax, x_data, y_data, xlim=None, ylim=None):
+	pass
 
 
 
